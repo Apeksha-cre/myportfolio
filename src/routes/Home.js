01b821416aacf41d960714a2c4dcf7955255f1a1
apeksha <apekshashah54@gmail.com>
2023-07-01 12:23:05 -0700

    Added components, images and style

---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import MainPageImg from '../components/MainPageImg'
+
+const Home = () => {
+  return (
+    <div>
+            <Navbar/>              {/*linking the nevigation bar*/}
+            <MainPageImg/>                                            
+    </div>
+  )
+}
+
+export default Home
